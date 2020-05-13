96d6a2f1d0b5bb0cca540916b80583c64e710881
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2020-05-13 20:56:25 +0200

    Nothing intresting xd little changes"

--- blender/chart.py
+++ blender/chart.py
@@ -1,19 +1,16 @@
 
-max_height_point = 3
-max_scene_width = 6
+MAX_HEIGHT_POINT = 3
+MAX_SCENE_WIDTH = 6
 
 class ColumnChart(object):
 
     def __init__(self, data):
         self.highest_value = self.find_max_value(data)
         self.columns = self.create_columns(data)
-        print(data)
-        print(self.highest_value)
-        print(self.columns[3].depth)
 
     def create_columns(self, data):
         columns = []
-        width = max_scene_width / (len(data) + 4)
+        width = MAX_SCENE_WIDTH / (len(data) + 4)
         depth = width * 0.66
 
         for row in data:
@@ -37,9 +34,12 @@ class ColumnChart(object):
         pass
 
 class Column(object):
+    width = ''
+    depth = ''
+
     def __init__(self, text, value):
         self.text = text
         self.value = value
 
     def calculate_height(self, highest_value):
-        self.hight = (self.value * max_height_point) / highest_value
+        self.hight = (self.value * MAX_HEIGHT_POINT) / highest_value
